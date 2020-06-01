af1569bf0807901566b6cb5e99e5622d236f1f23
laughinglyz <53049496+laughinglyz@users.noreply.github.com>
2020-06-01 21:06:22 +0800

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -13,7 +13,7 @@ from flask import Flask, jsonify, request
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="king125001",
+    passwd="",
     database="blockchain2",
     auth_plugin='mysql_native_password'
 )
